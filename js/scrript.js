c157e57923c4e60f5879cd554aa69ae016863156
Sajid-Rahman654 <godfather109238@gmail.com>
2023-11-14 15:45:06 +0600

    first commit

---
+++ js/scrript.js
@@ -0,0 +1,17 @@
+
+$('.dest-slide').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    centerMode:true,
+    centerPadding:'0px'
+  });
+  
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
